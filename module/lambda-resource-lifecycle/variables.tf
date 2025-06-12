887d7f3d5f1133182e674b5525361c8254ab44c6
Marjorie Echu <marjbogo@gmail.com>
2025-06-12 07:19:50 +0100

    variables addition

--- module/lambda-resource-lifecycle/variables.tf
+++ module/lambda-resource-lifecycle/variables.tf
@@ -17,3 +17,23 @@ variable "tags" {
   type        = map(string)
   description = "A map of key-value pairs representing common tags to apply to AWS resources (such as Name, Environment). Tags help in organizing and identifying resources, especially in large-scale environments."
 }
+
+variable "cost_threshold" {
+  description = "Maximum allowed EC2 instance cost in USD before termination"
+  type        = number
+}
+
+variable "micro_max_age_days" {
+  description = "Allowed age in days for t2.micro instances"
+  type        = number
+}
+
+variable "mattermost_webhook_url" {
+  description = "mattermost ec2 lifecyle webhook url"
+  type        = string
+}
+
+variable "default_max_age_days" {
+  description = "Allowed age in days for other EC2 instances"
+  type        = number
+}
